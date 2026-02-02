0bdd703a5c84cfe8a8eec3442b349c59bc62837f
Kasfia Mostafa <kasfiamostafa03@gmail.com>
2026-02-02 11:32:45 +0600

    code box project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='font-game'>Hello, Code Box!</h1>
+      <Button className='font-inter'>Click Me</Button>
+    </div>
+  )
+}
+
+export default page
